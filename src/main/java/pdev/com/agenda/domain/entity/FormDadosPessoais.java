73382e5300d067475b053c0bc1e531af22db9951
henrique.araujo <henrique.araujo@montreal.com.br>
2025-06-10 10:43:41 -0300

    migrations entities

---
+++ src/main/java/pdev/com/agenda/domain/entity/FormDadosPessoais.java
@@ -0,0 +1,63 @@
+package pdev.com.agenda.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Table(name = "fomulario_dados_pessoais")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FormDadosPessoais {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "fullname")
+    private String fullName;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "cpfBolsista")
+    private String cpfBolsista;
+
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "numEducasenso")
+    private String numEducasenso;
+
+    @Column(name = "PCD")
+    private String pcd;
+}
---
+++ src/main/java/pdev/com/agenda/domain/entity/FormEnderecoCandidato.java
@@ -0,0 +1,67 @@
+package pdev.com.agenda.domain.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "endereco_candidatos")
+@Getter
+@Setter
+public class FormEnderecoCandidato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "ponto_referencia")
+    private String pontoReferencia;
+
+    @Column(name = "residencia")
+    private String residencia;
+
+    @Column(name = "transporte_educacional")
+    private String transporteEducacional;
+
+    @Column(name = "tempo_deslocamento")
+    private String tempoDeslocamento;
+
+    @Column(name = "atividades_contraturno")
+    private String atividadesContraturno;
+
+    @Column(name = "telefone_residencial")
+    private String telefoneResidencial;
+
+    @Column(name = "telefone_trabalho")
+    private String telefoneTrabalho;
+
+    @Column(name = "telefone_celular")
+    private String telefoneCelular;
+
+    @Column(name = "email_confirmacao")
+    private String emailConfirmacao;
+
+    @Column(name = "responsavel_legal")
+    private String responsavelLegal;
+
+    @Column(name = "segmento_2025")
+    private String segmento2025;
+}
