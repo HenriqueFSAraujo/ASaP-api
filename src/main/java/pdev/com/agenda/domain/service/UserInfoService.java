ed7e6a377e1c4e2655f3d4fbb4db4bba7105a65b
henrique.araujo <henrique.araujo@montreal.com.br>
2025-06-22 22:51:20 -0300

    Ajuste retorno create user

--- src/main/java/pdev/com/agenda/domain/service/UserInfoService.java
+++ src/main/java/pdev/com/agenda/domain/service/UserInfoService.java
@@ -36,7 +36,6 @@ public class UserInfoService {
 
     public UserInfoDTO create(UserInfoDTO dto) {
         UserInfo entity = mapper.toEntity(dto);
-        entity.isFirstLogin(true);
         return mapper.toDTO(repository.save(entity));
     }
 
