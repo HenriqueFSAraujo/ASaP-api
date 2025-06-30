ce26f1dcfe9b9199d2813138e7fc7476a1fb5257
henrique.araujo <henrique.araujo@montreal.com.br>
2025-06-29 22:04:36 -0300

    Novas relações de tabelas com userid

--- src/main/resources/db/migration/V20__alter_table_create_user_id.sql
+++ src/main/resources/db/migration/V20__alter_table_create_user_id.sql
@@ -2,4 +2,8 @@
 ALTER TABLE processo_de_bolsa
 ADD COLUMN user_id BIGINT;
 
-
+-- Cria a foreign key referenciando userinfo(id)
+ALTER TABLE processo_de_bolsa
+ADD CONSTRAINT fk_processo_de_bolsa_user
+FOREIGN KEY (user_id)
+REFERENCES userinfo(id);
