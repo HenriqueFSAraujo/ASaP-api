d6a0077a03cb6224494da6051a1d4d41b512ae76
henrique.araujo <henrique.araujo@montreal.com.br>
2025-06-30 00:01:06 -0300

    new integrations forms

--- src/main/resources/db/migration/V25__alter_table_form_condicoes_habitacionais.sql
+++ src/main/resources/db/migration/V25__alter_table_form_condicoes_habitacionais.sql
@@ -1,7 +1,3 @@
 ALTER TABLE form_condicoes_habitacionais
 ADD COLUMN user_id BIGINT NOT NULL;
 
-ALTER TABLE form_condicoes_habitacionais
-ADD CONSTRAINT fk_form_condicoes_habitacionais_user
-FOREIGN KEY (user_id)
-REFERENCES userinfo(id);
