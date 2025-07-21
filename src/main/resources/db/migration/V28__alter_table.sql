350a7b1b254b2363cd8e2efe50999ef2f880f500
henrique.araujo <henrique.araujo@montreal.com.br>
2025-07-21 19:25:42 -0300

    migration create parecer socioeconomico

--- src/main/resources/db/migration/V28__alter_table.sql
+++ src/main/resources/db/migration/V28__alter_table.sql
@@ -1,6 +1,6 @@
 ALTER TABLE parecer_socioeconomico
     ALTER COLUMN nome_aluno TYPE VARCHAR(250),
-    ALTER COLUMN segmento_cursar2025 TYPE VARCHAR(250),
+    ALTER COLUMN segmento_cursar_2025 TYPE VARCHAR(250),
     ALTER COLUMN nome_responsavel TYPE VARCHAR(250),
     ALTER COLUMN cpf_responsavel TYPE VARCHAR(250),
     ALTER COLUMN telefone_responsavel TYPE VARCHAR(250),
