faddb7bc8174871d04641c8e652a5aaa38a17883
henrique.araujo <henrique.araujo@montreal.com.br>
2025-07-21 18:59:50 -0300

    migration create parecer socioeconomico

--- src/main/resources/db/migration/V27__create_parecer.sql
+++ src/main/resources/db/migration/V27__create_parecer.sql
@@ -1,5 +1,5 @@
 CREATE TABLE parecer_socioeconomico (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
 
     nome_aluno VARCHAR(255),
     data_nascimento_aluno DATE,
@@ -7,21 +7,20 @@ CREATE TABLE parecer_socioeconomico (
     nome_responsavel VARCHAR(255),
     cpf_responsavel VARCHAR(20),
     telefone_responsavel VARCHAR(20),
-    renda_bruta_familiar DECIMAL(12,2),
-    quantidade_pessoas_familia INT,
-    renda_per_capita DECIMAL(12,2),
-    renda_per_capita_salario_minimo DECIMAL(12,2),
-    percentual_lc_187 DOUBLE,
+    renda_bruta_familiar NUMERIC(12,2),
+    quantidade_pessoas_familia INTEGER,
+    renda_per_capita NUMERIC(12,2),
+    renda_per_capita_salario_minimo NUMERIC(12,2),
+    percentual_lc_187 DOUBLE PRECISION,
     beneficiario_programa_renda BOOLEAN,
     reside_proximo_unidade_escolar BOOLEAN,
     candidato_com_deficiencia BOOLEAN,
     doenca_grave_ou_deficiencia_familiar BOOLEAN,
-    quantidade_menores_dezoito_anos INT,
+    quantidade_menores_dezoito_anos INTEGER,
     aspectos_relevantes TEXT,
     resultado_socioeconomico VARCHAR(255),
     data_finalizacao_parecer DATE,
 
     user_id BIGINT NOT NULL,
-
     CONSTRAINT fk_parecer_user FOREIGN KEY (user_id) REFERENCES user_info(id)
 );
