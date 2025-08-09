054ed9da33cd6f1ef838080718995f147cffab2b
henrique.araujo <henrique.araujo@montreal.com.br>
2025-08-09 14:02:03 -0300

    Mapping auth class to swagger

--- src/main/java/pdev/com/agenda/domain/controller/AuthController.java
+++ src/main/java/pdev/com/agenda/domain/controller/AuthController.java
@@ -1,5 +1,10 @@
 package pdev.com.agenda.domain.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,16 +18,24 @@ import pdev.com.agenda.domain.service.UserLoginService;
 
 @RestController
 @RequestMapping("api/auth")
+@Api(tags = "Autenticação", description = "Endpoints para login e autenticação de usuários")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<UserLoginResponse> login(@RequestBody UserLoginRequest loginRequest) {
+    @ApiOperation(value = "Efetua login", notes = "Autentica o usuário e retorna um token JWT")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Login realizado com sucesso"),
+            @ApiResponse(code = 400, message = "Requisição inválida"),
+            @ApiResponse(code = 401, message = "Credenciais inválidas")
+    })
+    public ResponseEntity<UserLoginResponse> login(
+            @ApiParam(value = "Credenciais do usuário", required = true)
+            @RequestBody UserLoginRequest loginRequest) {
+
         UserLoginResponse response = authService.login(loginRequest);
         return ResponseEntity.ok(response);
     }
-
-
 }
