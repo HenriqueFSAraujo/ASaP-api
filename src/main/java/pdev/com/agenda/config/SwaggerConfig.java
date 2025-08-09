01ad972bf98ad33f32d85308732411514e3c3f3e
henrique.araujo <henrique.araujo@montreal.com.br>
2025-08-09 15:03:53 -0300

    Mapping auth class to swagger

--- src/main/java/pdev/com/agenda/config/SwaggerConfig.java
+++ src/main/java/pdev/com/agenda/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("pdev.com.agenda.api"))
+                .apis(RequestHandlerSelectors.basePackage("pdev.com.agenda.domain.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
