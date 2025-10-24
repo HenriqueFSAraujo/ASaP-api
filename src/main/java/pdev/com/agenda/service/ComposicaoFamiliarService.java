f3bc311cb76ec7f0276cce605d43880cec41c411
henrique.araujo <henrique.araujo@montreal.com.br>
2025-10-24 20:43:26 -0300

    refactor saveAllForUser method to delete existing records before saving new ones in ComposicaoFamiliarService

--- src/main/java/pdev/com/agenda/service/ComposicaoFamiliarService.java
+++ src/main/java/pdev/com/agenda/service/ComposicaoFamiliarService.java
@@ -51,9 +51,16 @@ public class ComposicaoFamiliarService {
         Long userId = request.getUserInfoId();
         UserInfo userInfo = userInfoRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("UserInfo not found with id: " + userId));
+
+        // Delete existing records associated with the user
+        List<ComposicaoFamiliar> existingComposicoes = repository.findAllByUserInfo(userInfo);
+        repository.deleteAll(existingComposicoes);
+
+        // Create new records
         List<ComposicaoFamiliar> composicoes = ComposicaoFamiliarMapper.INSTANCE.toEntityList(request.getComposicaoFamiliar());
         composicoes.forEach(composicao -> composicao.setUserInfo(userInfo));
         List<ComposicaoFamiliar> savedComposicoes = repository.saveAll(composicoes);
+
         return ComposicaoFamiliarMapper.INSTANCE.toDTOList(savedComposicoes);
     }
 
