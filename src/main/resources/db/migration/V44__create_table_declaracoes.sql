3b84e7336a4f36439c223c06794d8ff3fe4e1ac8
henrique.araujo <henrique.araujo@montreal.com.br>
2025-10-24 21:03:37 -0300

    add Declaracao entity, repository, service, and controller for managing declarations

--- src/main/resources/db/migration/V44__create_table_declaracoes.sql
+++ src/main/resources/db/migration/V44__create_table_declaracoes.sql
@@ -1,9 +1,8 @@
 CREATE TABLE declaracoes (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nome_declarante VARCHAR(255) NOT NULL,
     rg_declarante VARCHAR(50) NOT NULL,
     cpf_declarante VARCHAR(50) NOT NULL,
     nome_aluno VARCHAR(255) NOT NULL,
     aceite_termos BOOLEAN NOT NULL
 );
-
