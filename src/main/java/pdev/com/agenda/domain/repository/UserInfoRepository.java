0425e17f5df20f00398bf99702f8c57f75913792
henrique.araujo <henrique.araujo@montreal.com.br>
2025-11-04 11:47:29 -0300

    add user relation to Declaracao and implement retrieval by user ID

--- src/main/java/pdev/com/agenda/domain/repository/UserInfoRepository.java
+++ src/main/java/pdev/com/agenda/domain/repository/UserInfoRepository.java
@@ -2,18 +2,12 @@ package pdev.com.agenda.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pdev.com.agenda.domain.UserInfoResponse;
 import pdev.com.agenda.domain.entity.UserInfo;
-
-import java.util.Optional;
 
 @Repository
 public interface UserInfoRepository  extends JpaRepository<UserInfo, Long> {
 
-       Optional<UserInfo> findByEmail(String email);
-
-       public Optional<UserInfoResponse> findByUserName(String userName);
-
        boolean existsByEmail(String email);
        boolean existsByUserName(String userName);
 }
+
---
+++ src/main/resources/db/migration/V50__add_json_fields_to_bens_posses.sql
@@ -0,0 +1,6 @@
+ALTER TABLE bens_posses
+ADD COLUMN veiculos_json TEXT,
+ADD COLUMN familiares_escola_json TEXT,
+ADD COLUMN pessoas_com_deficiencia_json TEXT,
+ADD COLUMN despesas_mensais_json TEXT;
+
